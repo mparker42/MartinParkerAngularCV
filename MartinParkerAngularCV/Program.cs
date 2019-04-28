1ab40d7d455654167e32cae363d805c808ffd20c
Martin Parker <mparker42@hotmail.co.uk>
2019-04-28 20:53:59 +0100

    Add app insights

--- MartinParkerAngularCV/Program.cs
+++ MartinParkerAngularCV/Program.cs
@@ -19,6 +19,7 @@ namespace MartinParkerAngularCV
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
