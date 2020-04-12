72ef55bb42b0101f5c369fc0fbe26c53258898c7
mparker42 <mparker42@hotmail.co.uk>
2020-04-12 16:41:25 +0100

    update @angular/material and @angular/cdk to V9

--- MartinParkerAngularCV/ClientApp/src/app/app.module.ts
+++ MartinParkerAngularCV/ClientApp/src/app/app.module.ts
@@ -13,7 +13,11 @@ import { NavigationMenuComponent } from './navigation-menu/navigation-menu.compo
 import { AboutComponent } from "./about/about.component";
 import { PortfolioComponent } from "./portfolio/portfolio.component";
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { APIRequestService } from './services/api-request-service';
 import { TranslationsService } from './services/translation-service';
 import { PortfolioService } from './services/portfolio-service';
